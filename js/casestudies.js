f5ab9c6805ff744cc58fb793d27da3f2a291c8b0
rchlwng <rachel@lu-wang.com>
2019-12-29 06:52:06 -0800

    switches

---
+++ js/casestudies.js
@@ -0,0 +1,12 @@
+$("#cs1").hover(function() {
+    $("#cs1image").css("opacity", "60%");
+}, function() {
+    $("#cs1image").css("opacity", "0%");
+});
+
+
+$("#cs2").hover(function() {
+    $("#cs2image").css("opacity", "60%");
+}, function() {
+    $("#cs2image").css("opacity", "0%");
+});
