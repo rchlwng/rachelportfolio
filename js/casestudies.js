52fe6b251194e627722053b477a6f92573dfb574
rchlwng <rachel@lu-wang.com>
2020-01-07 11:02:27 -0800

    FUCK SAFARI

--- js/casestudies.js
+++ js/casestudies.js
@@ -1,12 +1,12 @@
 $("#cs1").hover(function() {
-    $("#cs1image").css("opacity", "60%");
+    $("#cs1image").css("opacity", "0.6");
 }, function() {
-    $("#cs1image").css("opacity", "0%");
+    $("#cs1image").css("opacity", "0");
 });
 
 
 $("#cs2").hover(function() {
-    $("#cs2image").css("opacity", "60%");
+    $("#cs2image").css("opacity", ".6");
 }, function() {
-    $("#cs2image").css("opacity", "0%");
+    $("#cs2image").css("opacity", "0");
 });
